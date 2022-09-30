8d00a51dacd9dae07482a55d545f6fff28596742
Victoria El Bedewi <victoriaelbedewi@Victorias-Air.askey.com>
2022-09-30 15:13:38 -0700

    html routes, port listening

--- server.js
+++ server.js
@@ -4,3 +4,29 @@ const path = require ('path');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+let jsonNotes = require('./db/db.json');
+
+// GET HTML routes
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    console.log("NOTES!");
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+
+
+// PORT listen
+app.listen(PORT, () => {
+    console.log(`now listening on port:  ${PORT}!`);
+});
